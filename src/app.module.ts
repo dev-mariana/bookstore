740a2ec905c4b4d5b045ee96c3175a892c79fb54
Mariana Bastos <maribastossrodrigues@gmail.com>
2021-07-28 16:15:03 -0300

    add database and environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,27 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BooksController } from './books.controller';
 import { BooksService } from './books.service';
-
+import * as dotenv from 'dotenv';
+import { Book } from './book.model';
+dotenv.config();
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: process.env.DB_HOST,
+      port: 3306,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DATABASE,
+      autoLoadModels: true,
+      synchronize: true
+    }),
+    SequelizeModule.forFeature([Book])
+  ],
   controllers: [AppController, BooksController],
   providers: [AppService, BooksService],
 })
--- src/books.service.ts
+++ src/books.service.ts
@@ -4,9 +4,9 @@ import { Book } from './book.model';
 @Injectable()
 export class BooksService {
   books: Book[] = [
-    new Book('LIV01', 'TypeScript Book', 29.9),
-    new Book('LIV02', 'Java Book', 38.9),
-    new Book('LIV03', 'Angular Book', 25.0),
+    // new Book('LIV01', 'TypeScript Book', 29.9),
+    // new Book('LIV02', 'Java Book', 38.9),
+    // new Book('LIV03', 'Angular Book', 25.0),
   ];
 
   getAll(): Book[] {
